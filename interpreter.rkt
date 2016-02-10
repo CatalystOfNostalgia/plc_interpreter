34c2f36817daec3552fe1db775a662b35c9b9a1f
unknown <saw106@case.edu>
2016-02-10 18:28:31 -0500

    finished fleshing out M_state_statement conditions

--- interpreter.rkt
+++ interpreter.rkt
@@ -7,11 +7,11 @@
 (define M_state_statement
   (lambda (parse_tree state)
     (cond
-      ((eq? (first_symbol parse_tree) 'var))
-      ((eq? (first_symbol parse_tree) '=))
+      ((eq? (first_symbol parse_tree) 'var) (M_state_statement (cdr parse_tree) (M_state_init (rest_of_statement parse_tree) state)))
+      ((eq? (first_symbol parse_tree) '=) (M_state_statement (cdr parse_tree) (M_state_assign (rest_of_statement parse_tree) state)))
       ((eq? (first_symbol parse_tree) 'return) (M_val_expression (rest_of-statement) state)))
-      ((eq? (first_symbol parse_tree) 'if))
-      ((eq? (first_symbol parse_tree) 'while)))))
+      ((eq? (first_symbol parse_tree) 'if) (M_state_statement (cdr parse_tree) (M_state_if (rest_of_statement parse_tree) state)))
+      ((eq? (first_symbol parse_tree) 'while) (M_state_statement (cdr parse_tree) (M_state_while (rest_of_statement parse_tree) state)))))
 
 (define first_var caar)
 (define first_var_val cadr)
